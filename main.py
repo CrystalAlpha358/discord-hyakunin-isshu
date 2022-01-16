48908ae7dc39fa6d9b1a6224c8b8635751498552
CrystalAlpha358 <qchigu76@gmail.com>
2022-01-16 18:15:25 +0900

    Add: 最低限の問題生成機能を追加
    構文: /question [choices_cnt] [swapmode=True|False] [testmode] [ephemeral]
    ついでに PEP 8 に準拠するようにした (pycodestyle --max-line-length=99)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, sample as rndsample
 import json
 import os
 import re
@@ -10,10 +10,10 @@ from discord.ext import pages as Pages
 
 # Constants
 JSON = './hyakunin-isshu.json'
-GUILD_ID = [ int(os.getenv('DISCORD_GUILD_ID')) ] if True else None
+GUILD_ID = [int(os.getenv('DISCORD_GUILD_ID'))] if True else None
 BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 OPT = {
-    'testmode':  Option(
+    'testmode': Option(
         bool,
         'プリントに表記されている50首のみ対象にします(既定値: True)',
         required=False, default=True
@@ -26,11 +26,13 @@ OPT = {
 }
 PAGE_SIZE = 10
 
+
 # Definition
 def err(string: str, code: int) -> None:
     print(f'Error: {string}', file=sys.stderr)
     if code > 0:
         sys.exit(code)
+
 
 def expandDictInList(lis: list, base: list) -> dict:
     ret = {}
@@ -38,11 +40,12 @@ def expandDictInList(lis: list, base: list) -> dict:
         ret[key] = [dic.get(key) for dic in lis]
     return ret
 
+
 def getDataByNo(index: int, data: dict) -> dict:
     ret = {}
 
     for key in ['first', 'second', 'author']:
-        if not key in ret:
+        if key not in ret:
             ret[key] = {}
         for itemkey in ['origin', 'kana']:
             ret[key][itemkey] = data[key][itemkey][index]
@@ -52,13 +55,12 @@ def getDataByNo(index: int, data: dict) -> dict:
 
     return ret
 
+
 def makeEmbed(index: int, data: list) -> discord.Embed:
     dt = getDataByNo(index, data)
     au = dt['author']
 
-    embed = discord.Embed(
-        title=f"{dt['first']['origin']}\n{dt['second']['origin']}"
-    )
+    embed = discord.Embed(title=f"{dt['first']['origin']}\n{dt['second']['origin']}")
     embed.set_author(name=f"No. {index + 1}")
     embed.add_field(
         name='かな',
@@ -74,8 +76,17 @@ def makeEmbed(index: int, data: list) -> discord.Embed:
 
     return embed
 
+
 async def sendError(ctx, msg: str) -> None:
     await ctx.respond(f':no_entry_sign: {msg}', ephemeral=True)
+
+
+# Class
+class DropdownView(discord.ui.View):
+    def __init__(self, item):
+        super().__init__()
+        self.add_item(item)
+
 
 # Start
 print("Discord Hyakunin Isshu bot\nMade by CrystalAlpha358")
@@ -89,7 +100,6 @@ bot = discord.Bot(intents=intents)
 print('Loading JSON file...')
 try:
     with open(JSON, 'r') as f:
-        #JOBJECT = json.load(f)
         DATA = expandDictInList(json.load(f)['data'], [
             'first', 'second', 'begin', 'mean', 'author', 'test'
         ])
@@ -98,26 +108,29 @@ try:
 except Exception as e:
     err(f"Loading the JSON file {JSON}:\n{e}", 1)
 
+
 # Ready
 @bot.event
 async def on_ready():
     print('Ready')
 
+
 # Slash commands
 @bot.slash_command(guild_ids=GUILD_ID, name='random')
 async def isshu_random(
     ctx,
-    testmode:  OPT['testmode' ],
+    testmode: OPT['testmode'],
     ephemeral: OPT['ephemeral']
 ):
     """ランダムに一句を選び詳細を表示します"""
     no = DATA['test'][randint(0, len(DATA['test']) - 1)] if testmode else randint(0, 99)
     await ctx.respond(embed=makeEmbed(no, DATA), ephemeral=ephemeral)
 
+
 @bot.slash_command(guild_ids=GUILD_ID, name='get')
 async def isshu_getByNo(
     ctx,
-    number:    Option(int,  '歌番号を指定します(1~100)', min_value=1, max_value=100),
+    number: Option(int,  '歌番号を指定します(1~100)', min_value=1, max_value=100),
     ephemeral: OPT['ephemeral']
 ):
     """歌番号から句を取得し詳細を表示します"""
@@ -126,11 +139,12 @@ async def isshu_getByNo(
     else:
         await ctx.respond(embed=makeEmbed(number - 1, DATA), ephemeral=ephemeral)
 
+
 @bot.slash_command(guild_ids=GUILD_ID, name='search')
 async def isshu_search(
     ctx,
-    regex:     Option(str, '検索文字列を指定します。句は歴史的仮名遣いです。正規表現が使えます'),
-    target:    Option(
+    regex: Option(str, '検索文字列を指定します。句は歴史的仮名遣いです。正規表現が使えます'),
+    target: Option(
         str, '検索対象を指定します(既定値: phrase)',
         required=False, default='phrase', choices=[
             'phrase', 'first_phrase', 'second_phrase',
@@ -139,7 +153,7 @@ async def isshu_search(
             'author', 'author/kana'
         ]
     ),
-    testmode:  OPT['testmode' ],
+    testmode: OPT['testmode'],
     ephemeral: OPT['ephemeral']
 ):
     """文字列から句を検索します"""
@@ -152,7 +166,7 @@ async def isshu_search(
         data = DATA['author'][kana]
     elif (pos := target.find('phrase')) >= 0:
         if pos == 0 or target.startswith('first_'):
-            data  = DATA['first'][kana]
+            data = DATA['first'][kana]
         if pos == 0 or target.startswith('second_'):
             data += DATA['second'][kana]
     else:
@@ -165,12 +179,12 @@ async def isshu_search(
         regex = re.compile(regex)
     except re.error as e:
         msg = '**正規表現エンジンがエラーを報告しました。文法が誤っていないか確認してください**\n'
-        if not e.pos is None:
+        if e.pos is not None:
             start = e.pos - 9 if len(e.pattern) >= 10 else 0
-            msg += f'位置 {e.pos}: ``' + \
-                   ('...' if start else '') + \
-                   e.pattern[start:e.pos + 1].replace('``', r'\`\`') + \
-                   '`` *←[問題箇所]*\n'
+            msg += (f'位置 {e.pos}: ``'
+                    + ('...' if start else '')
+                    + e.pattern[start:e.pos + 1].replace('``', r'\`\`')
+                    + '`` *←[問題箇所]*\n')
         msg += f'メッセージ:\n```\n{e.msg}\n```'
         await sendError(ctx, msg)
         return
@@ -194,15 +208,71 @@ async def isshu_search(
     pages = []
     for start in range(0, len(result), PAGE_SIZE):
         embed = discord.Embed(title=f':mag: 検索結果: {result_len}件')
-        for idx in result[start:start + PAGE_SIZE]:
+        for idx in result[start: start + PAGE_SIZE]:
             embed.add_field(
-                name=(DATA['first']['origin'][idx] + DATA['second']['origin'][idx]).replace(' ', ''),
+                # noqa
+                name=(DATA['first']['origin'][idx] + DATA['second']['origin'][idx])
+                     .replace(' ', ''),
                 value=f"No. {idx + 1}・{DATA['author']['origin'][idx]}",
                 inline=False
             )
         pages.append(embed)
 
     await Pages.Paginator(pages=pages).respond(ctx.interaction, ephemeral=ephemeral)
+
+
+@bot.slash_command(guild_ids=GUILD_ID, name='question')
+async def isshu_question(
+    ctx,
+    choices_cnt: Option(
+        int, '選択肢の数を指定します(2~10, 既定値: 4)',
+        required=False, min_value=2, max_value=10, default=4
+    ),
+    swapmode: Option(
+        bool, '下の句から上の句を選ぶモードにします(既定値: False)',
+        required=False, default=False
+    ),
+    testmode: OPT['testmode'],
+    ephemeral: OPT['ephemeral']
+):
+    """ランダムに問題を生成します"""
+    baselist = [i for i, v in enumerate(DATA['test']) if v] if testmode else range(100)
+    choices = rndsample(baselist, choices_cnt)
+    correct = randint(0, choices_cnt - 1)
+
+    class Question(discord.ui.Select):
+        def __init__(self):
+            super().__init__(placeholder='句を選択...', min_values=1, max_values=1)
+
+            i = 1
+            for idx in choices:
+                self.add_option(
+                    # noqa
+                    label=f"{i}._{DATA['first' if swapmode else 'second']['origin'][idx]}"
+                          .replace(' ', '').replace('_', ' '),
+                    value=str(i - 1)
+                )
+                i += 1
+
+        async def callback(self, interaction: discord.Interaction):
+            msg = ':o: 正解！' if int(self.values[0]) == correct else ':x: 不正解...'
+            await interaction.response.send_message(
+                content=msg,
+                embed=makeEmbed(choices[correct], DATA),
+                ephemeral=True
+            )
+
+    await ctx.respond(
+        embed=discord.Embed(
+            # noqa
+            title=f"Q._{DATA['second' if swapmode else 'first']['origin'][choices[correct]]}"
+                  .replace(' ', '').replace('_', ' '),
+            description=f"この{'下' if swapmode else '上'}の句に対応する"
+                        + f"__**{'上' if swapmode else '下'}の句**__を選択してください"
+        ),
+        view=DropdownView(Question()),
+        ephemeral=ephemeral
+    )
 
 # Connect
 print('Connecting...')
